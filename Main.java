24d70b03e2ede8b084517cd4c054d082522e0e7f
briuno <brunoalvesbezerra.ba@gmail.com>
2024-09-17 15:41:19 -0300

    ;

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 package LotofacilProject;
 
-import LotofacilProject.view.LoginView;
-
 
 public class Main {
     public static void main(String[] args) {
-        LoginView loginView = new LoginView();
-        loginView.setVisible(true);
+        
     }
     
 }
